722dfef75b84f685a2fb4fa8ec9ab6a65813c9d6
kolserdav <serega12101983@gmail.com>
2021-10-16 00:50:48 +0700

    Fixed bug file permissions

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,8 @@ switch(process.argv[2]) {
     data = fs.readFileSync(filePath).toString();
     data = '#!/usr/bin/env node\n' + data;
     fs.writeFileSync(filePath, data);
+    fs.chmodSync(filePath, '755');
+    console.info(`Set up env for file ${filePath}`);
     break;
   default:
 }
