6cb55479cb97fd5a51174c9fcc2547f027852e81
kolserdav <serega12101983@gmail.com>
2021-10-26 23:09:32 +0700

    Fixed deploy bug

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const arg = process.argv[2];
 let data, filePath;
 switch(process.argv[2]) {
   case 'env':
-    filePath = path.resolve(__dirname, '../bin/file-headers.js');
+    filePath = path.resolve(__dirname, '../bin/fhead.js');
     data = fs.readFileSync(filePath).toString();
     data = '#!/usr/bin/env node\n' + data;
     fs.writeFileSync(filePath, data);
